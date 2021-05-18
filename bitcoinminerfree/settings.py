1ec002642179b39f400e41b8896fea1efa91e949
Manouchehr Alapour <manuchian@yahoo.com>
2021-05-18 12:51:06 +0300

    parranuksia

--- bitcoinminerfree/settings.py
+++ bitcoinminerfree/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=yl=b-rjsono594!l74t#nqx4x(@c0-)12^oi#0^zha06g^eif'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['bitcoinminerfree.herokuapp.com',
